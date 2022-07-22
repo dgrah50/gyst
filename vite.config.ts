3cdbb8687d82ef40f79f5c6676ade20e30289f07
dgrah50 <d.graham50@hotmail.co.uk>
2022-07-22 01:12:46 +0100

    Add components alias

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      app: resolve(__dirname, 'src', 'app'),
-      components: resolve(__dirname, 'src', 'components'),
-      hooks: resolve(__dirname, 'src', 'hooks'),
+      '@components': resolve(__dirname, 'src', 'components'),
     },
   },
   css: {
